2aab39cac0010449c6f58b96f240d58196aa5618
wildenali <wildenwildenaliali@gmail.com>
2020-09-21 11:39:20 +0700

    How to use Http and show halow dunia in web browser as response

--- mywebsite/mywebsite/urls.py
+++ mywebsite/mywebsite/urls.py
@@ -16,6 +16,13 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from django.http import HttpResponse
+
+# method view
+def index(request):
+    return HttpResponse("Halow dunia")
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', index),
 ]
